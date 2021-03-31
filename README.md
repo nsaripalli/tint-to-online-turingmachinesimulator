81aad83ca6782ade20ac41da07d5fe877e8eec5e
nsaripalli <nithinsaripalli@gmail.com>
2021-03-31 11:51:41 -0400

    Inital Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a simple script to convert [tint](https://github.com/cjcodell1/tint) turing machine yaml files into and format
+that can be inserted into [turingmachinesimulator.com](https://turingmachinesimulator.com/) to help with the debugging of
+turing machines.
+
+Currently, only tested with one way 1 tape.
---
+++ main.py
@@ -0,0 +1,34 @@
+# /usr/bin/python
+import yaml
+
+
+# This is going to translate the tint format for this online tool:
+# https://turingmachinesimulator.com/
+def translate_tint_to_the_online_tm_format(name):
+    machine = None
+    with open(name, 'r') as stream:
+        try:
+            machine = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    direction_translations = {'L': '>', 'R': '<', 'S': '-'}
+
+    start = machine.get('start')
+    accept = machine.get('accept')
+    new_transitions = []
+    for i in machine.get('transitions'):
+        current_state = i[0]
+        read_symbol = i[1]
+        new_state = i[2]
+        write_symbol = i[3]
+        direction = direction_translations[i[4]]
+        new_transitions.append(f"{current_state},{read_symbol}\n{new_state},{write_symbol},{direction}\n\n")
+
+    final = f"name: {name}\ninit: {start}\naccept: {accept} \n{str().join(new_transitions)}"
+
+    with open(f"converted-{name}", 'w') as output:
+        output.write(final)
+
+
+if __name__ == '__main__':
+    translate_tint_to_the_online_tm_format('q6.yaml')
